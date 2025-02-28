b2a729b688f14cc6ac255f4d4d671adf7cb6c893
LN <1404199069@qq.com>
2025-02-28 13:50:16 +0800

    修改了 _document.tsx 文件，例如：添加了 GA 跟踪代码

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,20 @@ export default function Document() {
   return (
     <Html lang="zh-CN">
       <Head>
+        {/* Google Analytics 代码 */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-DBQLTW3EBH"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-DBQLTW3EBH');
+          `}
+        </Script>
         <Script
           id="baidu-hm"
           strategy="afterInteractive"
